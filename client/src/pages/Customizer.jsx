9fa55d15a2fe76a8ed52e9152f440c905f69109a
emrecikisir <138903572+emrecikisir@users.noreply.github.com>
2023-07-07 22:46:05 +0300

    Update Customizer.jsx

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -52,7 +52,7 @@ const Customizer = () => {
     try {
       setGeneratingImg(true);
 
-      const response = await fetch('http://localhost:8080/api/v1/dalle', {
+      const response = await fetch('http://ai-tshirt-aft1.onrender.com/api/v1/dalle', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
